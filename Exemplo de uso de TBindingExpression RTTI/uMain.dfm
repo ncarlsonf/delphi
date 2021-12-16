cec67bab78a75369409546d8afbf9281bd332c1e
Nelson Carlson Filho <ncarlsonf@gmail.com>
2021-12-15 21:10:02 -0300

    O uso de TBindingExpression irá mudar muito a forma com a qual escrevemos nossos códigos.  Aguardem!

--- Exemplo de uso de TBindingExpression RTTI/uMain.dfm
+++ Exemplo de uso de TBindingExpression RTTI/uMain.dfm
@@ -1,50 +1,85 @@
 object fmMain: TfmMain
   Left = 0
   Top = 0
+  BorderWidth = 5
   Caption = 'Exemplo de uso de TBindingExpression / RTTI'
-  ClientHeight = 429
-  ClientWidth = 916
+  ClientHeight = 560
+  ClientWidth = 906
   Color = clBtnFace
-  Font.Charset = DEFAULT_CHARSET
+  Font.Charset = ANSI_CHARSET
   Font.Color = clWindowText
-  Font.Height = -11
-  Font.Name = 'Tahoma'
+  Font.Height = -13
+  Font.Name = 'Segoe UI'
   Font.Style = []
   OldCreateOrder = False
   Position = poScreenCenter
   OnCreate = FormCreate
   PixelsPerInch = 96
-  TextHeight = 13
+  TextHeight = 17
   object Label1: TLabel
-    Left = 8
-    Top = 5
-    Width = 54
-    Height = 13
+    Left = 0
+    Top = 0
+    Width = 906
+    Height = 17
+    Align = alTop
     Caption = 'Express'#227'o:'
+    ExplicitWidth = 63
   end
   object Label2: TLabel
-    Left = 8
-    Top = 211
-    Width = 30
-    Height = 13
+    Left = 0
+    Top = 373
+    Width = 906
+    Height = 17
+    Align = alBottom
     Caption = 'Sa'#237'da:'
+    ExplicitTop = 292
+    ExplicitWidth = 35
   end
   object Label3: TLabel
-    Left = 8
-    Top = 400
-    Width = 617
-    Height = 13
+    Left = 0
+    Top = 543
+    Width = 906
+    Height = 17
+    Align = alBottom
     Caption = 
       'O uso de TBindingExpression no c'#243'digo abre a possibilidade de in' +
       'ferir transforma'#231#245'es e modificar c'#225'lculos em tempo de execu'#231#227'o'
+    ExplicitTop = 462
+    ExplicitWidth = 772
+  end
+  object Splitter1: TSplitter
+    Left = 0
+    Top = 332
+    Width = 906
+    Height = 3
+    Cursor = crVSplit
+    Align = alBottom
+    ExplicitTop = 17
+    ExplicitWidth = 247
   end
   object MemoExpr: TMemo
-    Left = 8
-    Top = 20
-    Width = 893
-    Height = 169
+    Left = 0
+    Top = 17
+    Width = 906
+    Height = 315
+    Align = alClient
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -13
+    Font.Name = 'Lucida Console'
+    Font.Style = []
     Lines.Strings = (
+      'fn.Variables.Clear()'
       'fn.Erase()'
+      'AddVar("$empresa$", "TK2000")'
+      'AddVar("$diretor$", "Vilmar Teixeira")'
+      'AddVar("$desenvolvedor$", "Nelson Carlson F.")'
+      'AddVar("$ano$", "2021")'
+      
+        '"Nossa empresa '#233' a" + $empresa$ + ". Nosso diretor '#233' o " + $dire' +
+        'tor$ + ". Um de nossos desenvolvedores '#233' o " + $desenvolvedor$'
+      '"O pr'#243'ximo ano ser'#225' o de " + (ToFloat($ano$) + 1)'
+      ''
       
         '"Hello world! Hello world! Hello world! Al'#244' mundo! Al'#244' mundo! Al' +
         #244' mundo!!!!!"'
@@ -58,24 +93,52 @@ object fmMain: TfmMain
       
         'person.Name + " tem " + fn.ToStr(person.Age) + " anos. O dobro d' +
         'a idade de " + person.Name + " '#233' " + fn.ToStr(person.Age * 2) + ' +
-        '" anos de idade."')
+        '" anos de idade."'
+      'fn.GetRandom() / 14 + 1 + " Hello!"'
+      'fn.Sum([1.3, 4.2, 7]) + 2'
+      'fn.Avg([9.5, 2, 9.5])'
+      'fn.Round(1500/7, 2)'
+      'fn.Values.Clear()'
+      'fn.Values.Add(4)'
+      'fn.Values.Add(2)'
+      'fn.Values.Add(5)'
+      'fn.Values.Items[2]'
+      'Sum(fn.Values.ToArray())'
+      ''
+      '')
+    ParentFont = False
+    ScrollBars = ssBoth
     TabOrder = 0
     WordWrap = False
+    ExplicitHeight = 234
   end
   object MemoOut: TMemo
-    Left = 8
-    Top = 226
-    Width = 893
-    Height = 169
+    Left = 0
+    Top = 390
+    Width = 906
+    Height = 153
+    Align = alBottom
+    ScrollBars = ssBoth
     TabOrder = 1
+    ExplicitTop = 309
   end
-  object Button1: TButton
-    Left = 420
-    Top = 195
-    Width = 75
-    Height = 25
-    Caption = 'Avaliar'
+  object Panel1: TPanel
+    Left = 0
+    Top = 335
+    Width = 906
+    Height = 38
+    Align = alBottom
+    BevelOuter = bvNone
     TabOrder = 2
-    OnClick = Button1Click
+    ExplicitTop = 254
+    object Button1: TButton
+      Left = 0
+      Top = 4
+      Width = 75
+      Height = 25
+      Caption = 'Avaliar'
+      TabOrder = 0
+      OnClick = Button1Click
+    end
   end
 end
--- Exemplo de uso de TBindingExpression RTTI/uMain.pas
+++ Exemplo de uso de TBindingExpression RTTI/uMain.pas
@@ -5,7 +5,8 @@ interface
 uses
     Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
     System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
-    Vcl.StdCtrls, System.Bindings.Expression, System.Bindings.ExpressionDefaults;
+    Vcl.StdCtrls, System.Bindings.Expression, System.Bindings.ExpressionDefaults,
+    Vcl.ExtCtrls, System.Math;
 
 type
     TfmMain = class(TForm)
@@ -13,10 +14,12 @@ type
         MemoOut: TMemo;
         Label1: TLabel;
         Label2: TLabel;
+        Label3: TLabel;
+        Panel1: TPanel;
         Button1: TButton;
-    Label3: TLabel;
+        Splitter1: TSplitter;
         procedure Button1Click(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
+        procedure FormCreate(Sender: TObject);
     private
         { Private declarations }
     public
@@ -31,7 +34,7 @@ implementation
 {$R *.dfm}
 
 uses
-    TkHelpers;
+    TkHelpers, System.Generics.Collections;
 
 type
     TPerson = class
@@ -47,17 +50,65 @@ type
         property Age: Integer read FAge write FSetAge;
     end;
 
-    TMyFunct = class
+    TVariable = class(TObject)
+        Name: String;
+        Value: String;
+    end;
+
+    TMethods = class
+    private
+        FValues: TList<Extended>;
+        FVariables: TList<TVariable>;
+        procedure FSetValues(values: TList<Extended>);
+        function FGetValues(): TList<Extended>;
+        procedure FSetVariables(variables: TList<TVariable>);
+        function FGetVariables(): TList<TVariable>;
     public
+        constructor Create();
+        destructor Destroy();
+
+        function VTrue(): Boolean;
+        function VFalse(): Boolean;
         function ToStr(i: Double): String;
+        function ToFloat(i: String): Extended;
+        function GetRandom(): Double; overload;
+        function GetRandom(seed: Int64; divBy: Int64): Double; overload;
+        function GetRandom(seed: Int64): Double; overload;
+        function Power(num, pow: Extended): Extended;
+        function Sqrt(num: Extended): Extended;
+        function Sum(values: String): Extended;
+        function Max(values: String): Extended;
+        function Min(values: String): Extended;
+        function Avg(values: String): Extended;
+        function Round(num: Extended; digits: Integer): Extended;
+        function Summation(numBegin, numEnd, Step: Extended): Extended;
+        function AddVar(name: String; value: String): String;
+
         function Erase(): String;
         function Notepad(): String;
+        function IIf(expr: Boolean; a, b: Variant): String;
+        function ValuesToString(): String;
+        property Values: TList<Extended> read FGetValues write FSetValues;
+        property Variables: TList<TVariable> read FGetVariables write FSetVariables;
     end;
 
 var
     sOut: String;
     person: TPerson;
-    myFunct: TMyFunct;
+    methods: TMethods;
+    vTrue, vFalse: Boolean;
+
+procedure TfmMain.FormCreate(Sender: TObject);
+begin
+    person := TPerson.Create();
+    person.Name := 'Nelson';
+    person.Age := 42;
+
+    methods := TMethods.Create();
+    methods.Values.Add(1);
+    methods.Values.Add(2);
+    methods.Values.Add(3);
+end;
 
 procedure TPerson.FSetName(name: String);
 begin
@@ -81,44 +132,341 @@ begin
     FAge := age;
 end;
 
-function TMyFunct.ToStr(i: Double): String;
+constructor TMethods.Create();
+begin
+    FValues := TList<Extended>.Create();
+    FVariables := TList<TVariable>.Create();
+end;
+
+destructor TMethods.Destroy();
+begin
+    FValues.Free();
+    FVariables.Free();
+end;
+
+function TMethods.ToStr(i: Double): String;
 begin
     Result := FloatToStr(i);
 end;
 
+function TMethods.ToFloat(i: String): Extended;
+begin
+    Result := StrToFloat(i);
+end;
 
-function TMyFunct.Erase(): String;
+function TMethods.GetRandom(seed: Int64; divBy: Int64): Double;
+begin
+    if divBy > 0 then
+        Result := Random(seed) / divBy
+    else
+        Result := Random(seed);
+end;
+
+function TMethods.GetRandom(seed: Int64): Double;
+begin
+    Result := GetRandom(seed, 10000);
+end;
+
+function TMethods.GetRandom(): Double;
+begin
+    Result := GetRandom(1000000000, 10000);
+end;
+
+function TMethods.Power(num, pow: Extended): Extended;
+begin
+    Result := System.Math.Power(num, pow);
+end;
+
+function TMethods.Sqrt(num: Extended): Extended;
+begin
+    Result :=System.Sqrt(num);
+end;
+
+function TMethods.Sum(values: String): Extended;
+var
+    aStr: TArray<String>;
+    i: Integer;
+begin
+    Result := 0;
+    aStr := values.Split([',']);
+    for i := Low(aStr) to High(aStr) do
+    begin
+        Result := Result + StrToFloat(StringReplace(aStr[i], '.', ',', [rfReplaceAll]));
+    end;
+end;
+
+function TMethods.Max(values: String): Extended;
+var
+    aStr: TArray<String>;
+    i: Integer;
+    num: Extended;
+begin
+    Result := Extended.MinValue;
+    aStr := values.Split([',']);
+    for i := Low(aStr) to High(aStr) do
+    begin
+        num := StrToFloat(StringReplace(aStr[i], '.', ',', [rfReplaceAll]));
+        if num > Result then
+            Result := num;
+    end;
+end;
+
+function TMethods.Min(values: String): Extended;
+var
+    aStr: TArray<String>;
+    i: Integer;
+    num: Extended;
+begin
+    Result := Extended.MaxValue;
+    aStr := values.Split([',']);
+    for i := Low(aStr) to High(aStr) do
+    begin
+        num := StrToFloat(StringReplace(aStr[i], '.', ',', [rfReplaceAll]));
+        if num < Result then
+            Result := num;
+    end;
+end;
+
+function TMethods.Avg(values: String): Extended;
+var
+    aStr: TArray<String>;
+    i: Integer;
+begin
+    Result := 0;
+    aStr := values.Split([',']);
+    if Length(aStr) = 0 then
+        Exit;
+    for i := Low(aStr) to High(aStr) do
+    begin
+        Result := Result + StrToFloat(StringReplace(aStr[i], '.', ',', [rfReplaceAll]));
+    end;
+    Result := Result / Length(aStr);
+end;
+
+function TMethods.Erase(): String;
 begin
     fmMain.MemoOut.Text := '';
     sOut := '';
     Result := '';
 end;
 
-function TMyFunct.Notepad(): String;
+function TMethods.Round(num: Extended; digits: Integer): Extended;
+begin
+    digits := digits * -1;
+    Result := RoundTo(num, digits);
+end;
+
+function TMethods.Summation(numBegin, numEnd, step: Extended): Extended;
+var
+    i, num: Extended;
+begin
+    Result := 0;
+    if step <= 0 then
+        step := 1;
+    num := 0;
+    i := numBegin;
+    while i <= numEnd do
+    begin
+        Result := Result + i;
+        i := i + step;
+    end;
+end;
+
+function TMethods.AddVar(name: String; value: String): String;
+var
+    i: Integer;
+    exists: Boolean;
+    v: TVariable;
+begin
+    Result := '';
+    name := Trim(name);
+    if Length(name) < 3 then
+        Exit;
+
+    if not(name.StartsWith('$') and name.EndsWith('$')) then
+        Exit;
+
+    exists := False;
+    for i := 0 to Variables.Count - 1 do
+    begin
+        if Trim(LowerCase( Variables.Items[i].Name )) = Trim(LowerCase(name)) then
+        begin
+            exists := True;
+            Exit;
+        end;
+    end;
+    v := TVariable.Create();
+    v.Name := name;
+    v.Value := value;
+    Variables.Add(v);
+end;
+
+procedure TMethods.FSetValues(values: TList<Extended>);
+begin
+    FValues := values;
+end;
+
+function TMethods.FGetValues(): TList<Extended>;
+begin
+    Result := FValues;
+end;
+
+procedure TMethods.FSetVariables(variables: TList<TVariable>);
+begin
+    FVariables := variables;
+end;
+
+function TMethods.FGetVariables(): TList<TVariable>;
+begin
+    Result := FVariables;
+end;
+
+function TMethods.Notepad(): String;
 begin
     Result := '';
     TkHelpers.NotePad(sOut, True);
 end;
 
+function TMethods.IIf(expr: Boolean; a, b: Variant): String;
+begin
+    if expr then
+        Result := VarAsString(a)
+    else
+        Result := VarAsString(b);
+end;
+
+function TMethods.ValuesToString(): String;
+var
+    i: Integer;
+begin
+    Result := '';
+    for i := 0 to FValues.Count - 1 do
+    begin
+        if Result <> '' then
+            Result := Result + ',';
+        Result := Result + StringReplace(FloatToStr(FValues.Items[i]), ',', '.', [rfReplaceAll]);
+    end;
+end;
+
+function TMethods.VTrue(): Boolean;
+begin
+    Result := True;
+end;
+
+function TMethods.VFalse(): Boolean;
+begin
+    Result := False;
+end;
+
+function UpdFuncPart(expr, exprOri, exprSubst: String): String;
+begin
+    expr := ' ' + expr + ' ';
+    expr := StringReplace(expr, '(' + exprOri + '(', '(' + exprSubst + '(', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ' ' + exprOri + '(', ' ' + exprSubst + '(', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '+' + exprOri + '(', '+' + exprSubst + '(', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '-' + exprOri + '(', '-' + exprSubst + '(', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '*' + exprOri + '(', '*' + exprSubst + '(', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '/' + exprOri + '(', '/' + exprSubst + '(', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '=' + exprOri + '(', '=' + exprSubst + '(', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '<' + exprOri + '(', '<' + exprSubst + '(', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '>' + exprOri + '(', '>' + exprSubst + '(', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '^' + exprOri + '(', '^' + exprSubst + '(', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ',' + exprOri + '(', ',' + exprSubst + '(', [rfReplaceAll, rfIgnoreCase]);
+    Result := expr;
+end;
+
+function UpdBoolParts(expr: String): String;
+begin
+    expr := ' ' + expr + ' ';
+    expr := StringReplace(expr, '(true)', 'fn.VTrue()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '(true ', 'fn.VTrue()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ' true)', 'fn.VTrue()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '(true=', 'fn.VTrue()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '=true)', 'fn.VTrue()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ' true=', 'fn.VTrue()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '=true ', 'fn.VTrue()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ' true ', 'fn.VTrue()', [rfReplaceAll, rfIgnoreCase]);
+
+    expr := StringReplace(expr, '(true,', '(fn.VTrue(),', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ',true)', ',fn.VTrue())', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ' true,', ' fn.VTrue(),', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ',true ', ',fn.VTrue() ', [rfReplaceAll, rfIgnoreCase]);
+
+
+    expr := StringReplace(expr, '(false)', 'fn.VFalse()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '(false ', 'fn.VFalse()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ' false)', 'fn.VFalse()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '(false=', 'fn.VFalse()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '=false)', 'fn.VFalse()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ' false=', 'fn.VFalse()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, '=false ', 'fn.VFalse()', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ' false ', 'fn.VFalse()', [rfReplaceAll, rfIgnoreCase]);
+
+    expr := StringReplace(expr, '(false,', '(fn.VFalse(),', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ',false)', ',fn.VFalse())', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ' false,', ' fn.VFalse(),', [rfReplaceAll, rfIgnoreCase]);
+    expr := StringReplace(expr, ',false ', ',fn.VFalse() ', [rfReplaceAll, rfIgnoreCase]);
+
+    Result := expr;
+end;
+
 procedure TfmMain.Button1Click(Sender: TObject);
 var
     bindExpr: TBindingExpression;
-    i: Integer;
+    i, idx, p: Integer;
     s: String;
+    dict: TBindExprDict;
+    expr, expr2: String;
 begin
     try
         bindExpr := TBindingExpressionDefault.Create;
+        dict := TBindExprDict.Create('default');
         sOut := '';
         for i := 0 to MemoExpr.Lines.Count - 1 do
         begin
             if Trim(MemoExpr.Lines.Strings[i]) <> '' then
             begin
-                bindExpr.Source := MemoExpr.Lines.Strings[i];
-                bindExpr.Compile([
-                    TBindingAssociation.Create(person, 'person'),
-                    TBindingAssociation.Create(myFunct, 'fn')
-                ]);
-                s := bindExpr.Evaluate.GetValue.ToString();
+                expr := MemoExpr.Lines.Strings[i];
+                expr := UpdBoolParts(expr);
+                expr := UpdFuncPart(expr, 'iif', 'fn.IIf');
+                expr := UpdFuncPart(expr, 'power', 'fn.Power');
+                expr := UpdFuncPart(expr, 'sqrt', 'fn.Sqrt');
+                expr := UpdFuncPart(expr, 'sum', 'fn.Sum');
+                expr := UpdFuncPart(expr, 'avg', 'fn.Avg');
+                expr := UpdFuncPart(expr, 'min', 'fn.Min');
+                expr := UpdFuncPart(expr, 'max', 'fn.Max');
+                expr := UpdFuncPart(expr, 'round', 'fn.Round');
+                expr := UpdFuncPart(expr, 'summation', 'fn.Summation');
+                expr := UpdFuncPart(expr, 'tofloat', 'fn.ToFloat');
+                expr := UpdFuncPart(expr, 'fn.Values.ToString', 'fn.ValuesToString');
+                expr := UpdFuncPart(expr, 'fn.Values.ToArray', 'fn.ValuesToString');
+                expr := UpdFuncPart(expr, 'addvar', 'fn.AddVar');
+
+                for idx := 0 to methods.Variables.Count - 1 do
+                begin
+                    expr := StringReplace(expr, methods.Variables.Items[idx].Name, '"' + StringReplace(methods.Variables.Items[idx].Value, '"', '"+"', [rfReplaceAll]) + '"', [rfReplaceAll]);
+                end;
+
+                expr2 := StringReplace(expr, '[', '"', [rfReplaceAll]);
+                expr2 := StringReplace(expr2, ']', '"', [rfReplaceAll]);
+                try
+                    bindExpr.Source := expr;
+                    bindExpr.Compile([
+                        TBindingAssociation.Create(person, 'person'),
+                        TBindingAssociation.Create(methods, 'fn')
+                    ]);
+                    s := bindExpr.Evaluate.GetValue.ToString();
+                except
+                    on e: Exception do
+                    begin
+                        bindExpr.Source := expr2;
+                        bindExpr.Compile([
+                            TBindingAssociation.Create(person, 'person'),
+                            TBindingAssociation.Create(methods, 'fn')
+                        ]);
+                        s := bindExpr.Evaluate.GetValue.ToString();
+                    end;
+                end;
                 if Trim(s) <> '' then
                     sOut := sOut + s + #13#10;
             end;
@@ -126,16 +474,8 @@ begin
         MemoOut.Lines.Add(sOut);
     finally
         bindExpr.Free;
+        dict.Free();
     end;
-end;
-
-procedure TfmMain.FormCreate(Sender: TObject);
-begin
-    person := TPerson.Create();
-    person.Name := 'Nelson';
-    person.Age := 42;
-
-    myFunct := TMyFunct.Create();
 end;
 
 end.
